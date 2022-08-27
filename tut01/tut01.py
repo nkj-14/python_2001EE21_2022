a5d8d6ca80945809773a064eec9e4dda9d5eae03
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2022-08-27 15:40:43 +0530

    code submission for tut01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,9 @@
-def factorial(int x):
-    
+def factorial(x):
+    if x==0:
+        return 1
+    else:
+        return (x*factorial(x-1))
 
-n=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+x=int(input("Enter the number whose factorial is to be found"))
+ans=factorial(x)
+print("The factorial of given number", x, "is", ans, ".")
