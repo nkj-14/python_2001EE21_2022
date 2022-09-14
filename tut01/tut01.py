9a998da8a4ee3b288941cc58bdd02215ba3b4700
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2022-09-14 23:45:49 +0530

    checking for python version

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -4,6 +4,14 @@ import pandas as pd
 #for data preprocessing read all data
 data = pd.read_csv("octant_input.csv")
 
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+    
 def octact_identification(mod=5000):
 ###Code
 
