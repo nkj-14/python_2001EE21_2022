c432a6877b6e965dafb5192f3270a08abcca958f
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2022-10-19 07:10:24 +0530

    preprocessed the data and found out the individual octant id

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -18,7 +18,108 @@ def octant_range_names(mod=5000):
     try:
         data = pd.read_excel("octant_input.xlsx")
 
+    
+        cnt=0
+        o_1=o_2=o_3=o_4=o_5=o_6=o_7=o_8=0
 
+        d_2 = data.U
+        d_3 = data.V
+        d_4 = data.W
+        d1= {'+++': "+1", "++-": "-1", "-++": "+2", "-+-": "-2",
+            "--+": "+3", "---": "-3", "+-+": "+4", "+--": "-4"}
+        Octant = []
+        for r in range(len(d_2)):
+            if d_2[r]>0  and d_3[r]>0 and d_4[r]>0:
+                o_1=o_1+1
+            if d_2[r]>0  and d_3[r]>0 and d_4[r]<0:
+                o_2=o_2+1
+            if d_2[r]<0  and d_3[r]>0 and d_4[r]>0:
+                o_3=o_3+1
+            if d_2[r]<0  and d_3[r]>0 and d_4[r]<0:
+                o_4=o_4+1
+            if d_2[r]<0  and d_3[r]<0 and d_4[r]>0:
+                o_5=o_5+1
+            if d_2[r]<0  and d_3[r]<0 and d_4[r]<0:
+                o_6=o_6+1
+            if d_2[r]>0  and d_3[r]<0 and d_4[r]>0:
+                o_7=o_7+1
+            if d_2[r]>0  and d_3[r]<0 and d_4[r]<0:
+                o_8=o_8+1
+   
+        intial = data['U'].mean()
+        final = data['V'].mean()
+        acc = data['W'].mean()
+
+        data
+        o1=[intial]
+        o2=[final]
+        o3=[acc]
+
+        for i in range(len(data['Time'])-1):
+            o1.append(None)
+            o2.append(None)
+            o3.append(None)
+        d=[["Overall count"],[o_1, o_2, o_3, o_4, o_5, o_6, o_7, o_8]]
+        makedata = {"Time": data['Time'], "U":data["U"], "V":data["V"],
+                        "W":data["W"], "U Avg":o1,"V Avg": o2, "W Avg": o3}
+
+
+        j = r = 0
+
+        if r%mod==0:
+            o_1=o_2=o_3=o_4=o_5=o_6=o_7=o_8=0
+        if d_2[r]>0  and d_3[r]>0 and d_4[r]>0:
+            o_1=o_1+1
+        elif d_2[r]>0  and d_3[r]>0 and d_4[r]<0:
+            o_2=o_2+1
+        elif d_2[r]<0  and d_3[r]>0 and d_4[r]>0:
+            o_3=o_3+1
+        elif d_2[r]<0  and d_3[r]>0 and d_4[r]<0:
+            o_4=o_4+1
+        elif d_2[r]<0  and d_3[r]<0 and d_4[r]>0:
+            o_5=o_5+1
+        elif d_2[r]<0  and d_3[r]<0 and d_4[r]<0:
+            o_6=o_6+1
+        elif d_2[r]>0  and d_3[r]<0 and d_4[r]>0:
+            o_7=o_7+1
+        elif d_2[r]>0  and d_3[r]<0 and d_4[r]<0:
+            o_8=o_8+1
+        
+        w1 = [i-acc for i in data['W']]     
+        v1 = [i-final for i in data['V']]
+        u1 = [i-intial for i in data['U']]
+
+        pg = pd.DataFrame(makedata)
+            # make new columns of three variables
+        pg["U'=U-Uavg"] = u1
+        pg["V'=V-Vavg"] = v1
+        pg["W'=W-Wavg"] = w1
+
+        Octant = []
+        b1 = pg["V'=V-Vavg"].to_list()
+        c1 = pg["W'=W-Wavg"].to_list()
+        a1 = pg["U'=U-Uavg"].to_list()
+        for i in range(len(a1)):
+            s = ""
+            if (a1[i] < 0):
+                s += '-'
+            else:
+                s += '+'
+            if (b1[i] < 0):
+                s += '-'
+            else:
+                s += '+'
+            if (c1[i] < 0):
+                s += '-'
+            else:
+                s += '+'
+            Octant.append(d1[s])
+
+        pg["Octant"] = Octant
+        da = {"": [None], "OctantID": "Overall Count", 1: [Octant.count('+1')], -1: [Octant.count('-1')], 2: [Octant.count('+2')], -2: [Octant.count('-2')],
+                    3: [Octant.count('+3')], -3: [Octant.count('-3')], 4: [Octant.count('+4')], -4: [Octant.count('-4')]}
+
+        h1= pd.DataFrame(da)
 
     except:
         print("Something went wrong while opening the file or file is not found.")
