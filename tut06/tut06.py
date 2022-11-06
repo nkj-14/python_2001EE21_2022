8eebc5827eab2a26378d11b162f062ae4b88eba0
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2022-11-07 02:16:29 +0530

    data preprocessed and run a loop over dates to get the all monday and thursday

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -1,6 +1,7 @@
 
 
-from datetime import datetime
+from datetime import date,datetime,timedelta
+from tracemalloc import start
 start_time = datetime.now()
 
 def attendance_report():
@@ -12,6 +13,108 @@ def attendance_report():
     try:
         d1 = pd.read_csv("input_registered_students.csv")
         d2 = pd.read_csv("input_attendance.csv")
+
+        roll = d1["Roll No"]
+        name = d1["Name"]
+        timestamp = d2["Timestamp"]
+        attendance = d2["Attendance"]
+        attendance_roll = []
+        dates = []
+        dat = {}
+        din = {}
+
+        for i in range(len(timestamp)):
+            attendance_roll.append(attendance[i][0:8])
+
+        cc = 0
+        for i in range(len(timestamp[0])):
+            if(timestamp[0][i]=='/'):
+                cc=1
+                break
+            elif(timestamp[0][i]=='-'):
+                cc=2
+                break
+        if(cc==1):
+            f1 = "%d/%m/%Y %H:%M"
+            d = datetime.strptime(timestamp[0], f1)
+            dd = d.timetuple()
+            start_date = date(dd[0], dd[1], dd[2])
+            ee=0
+            for i in range(len(timestamp[len(timestamp)-1])):
+                if(timestamp[0][i]=='/'):
+                    ee=1
+                    break
+                elif(timestamp[0][i]=='-'):
+                    ee=2
+                    break
+            if(ee==1):
+                f2 = "%d/%m/%Y %H:%M"
+                t = datetime.strptime(timestamp[len(timestamp)-1], f2)
+                tt = t.timetuple()
+                end_date = date(tt[0], tt[1], tt[2])
+                delta = timedelta(days=1)
+                while start_date <= end_date:
+                    s=start_date.timetuple()
+                    if(s[6]==0 or s[6]==3):
+                        dates.append(start_date)
+                        dat[start_date] = []
+                        din[start_date] = []
+                    start_date += delta
+
+            elif(ee==2):
+                f2 = "%d-%m-%Y %H:%M"
+                t = datetime.strptime(timestamp[len(timestamp)-1], f2)
+                tt = t.timetuple()
+                end_date = date(tt[0], tt[1], tt[2])
+                delta = timedelta(days=1)
+                while start_date <= end_date:
+                    s=start_date.timetuple()
+                    if(s[6]==0 or s[6]==3):
+                        dates.append(start_date)
+                        dat[start_date] = []
+                        din[start_date] = []
+                    start_date += delta
+
+        elif(cc==2):
+            f1 = "%d-%m-%Y %H:%M"
+            t = datetime.strptime(timestamp[0], f1)
+            tt = t.timetuple()
+            start_date = date(tt[0], tt[1], tt[2])
+            ee=0
+            for i in range(len(timestamp[len(timestamp)-1])):
+                if(timestamp[0][i]=='/'):
+                    ee=1
+                    break
+                elif(timestamp[0][i]=='-'):
+                    ee=2
+                    break
+            if(ee==1):
+                f2 = "%d/%m/%Y %H:%M"
+                d = datetime.strptime(timestamp[len(timestamp)-1], f2)
+                dd = d.timetuple()
+                end_date = date(dd[0], dd[1], dd[2])
+                delta = timedelta(days=1)
+                while start_date <= end_date:
+                    s=start_date.timetuple()
+                    if(s[6]==0 or s[6]==3):
+                        dates.append(start_date)
+                        dat[start_date] = []
+                        din[start_date] = []
+                    start_date += delta
+
+            elif(ee==2):
+                f2 = "%d-%m-%Y %H:%M"
+                d = datetime.strptime(timestamp[len(timestamp)-1], f2)
+                dd = d.timetuple()
+                end_date = date(dd[0], dd[1], dd[2])
+                delta = timedelta(days=1)
+                while start_date <= end_date:
+                    s=start_date.timetuple()
+                    if(s[6]==0 or s[6]==3):
+                        dates.append(start_date)
+                        dat[start_date] = []
+                        din[start_date] = []
+                    start_date += delta
 
     except:
         print("Something went wrong while opening the file or file is not found.")
