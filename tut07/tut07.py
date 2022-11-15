1e9288bb1d98f8f92b917468eef5997c2be7d106
nkj-14 <jadhav_2001ee21@iitp.ac.in>
2022-11-16 00:58:55 +0530

    data processed and got octant of each individual

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -35,6 +35,53 @@ def octant_analysis(mod=5000):
 			d = pd.read_excel(file)
 
 
+			d_1 = d["T"]
+			d_2 = d["U"]
+			d_3 = d["V"]
+			d_4 = d["W"]
+			d_5= {"+++": 1, "++-": -1, "-++": 2, "-+-": -2,
+        		"--+": 3, "---": -3, "+-+": 4, "+--": -4}
+			
+			ini = round(d["U"].mean(),3)
+			mi = round(d["V"].mean(),3)
+			las = round(d["W"].mean(),3)
+			o1 = [ini]
+			o2 = [mi]
+			o3 = [las]
+			
+			for i in range(len(d["T"])-1):
+				o1.append(None)
+				o2.append(None)
+				o3.append(None)
+			
+			u1 = [round(i-ini,3) for i in d["U"]]
+			v1 = [round(i-mi,3) for i in d["V"]]
+			w1 = [round(i-las,3) for i in d["W"]]
+			
+			Octant = []
+			for i in range(len(u1)):
+				s = ""
+				if(u1[i]<0):
+					s += "-"
+				else:
+					s += "+"
+				if(v1[i]<0):
+					s += "-"
+				else:
+					s += "+"
+				if(w1[i]<0):
+					s += "-"
+				else:
+					s += "+"
+				Octant.append(d_5[s])
+				
+			col = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
+			text = ["T", "U", "V", "W", "U Avg", "V Avg", "W Avg", "U'=U-U Avg", "V'=V-V Avg", "W'=W-W Avg", "Octant"]
+			ri = 2
+			wh = [d_1, d_2, d_3, d_4, o1, o2, o3, u1, v1, w1, Octant]
+			
+
+
 
 			
 	except:
